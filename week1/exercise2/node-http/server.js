d7218752b52dc8629260ad9064453564da71fc92
tetraquard <tstanmaysaxena@gmail.com>
2016-04-12 12:19:00 +0530

    Basic Server on node

---
+++ week1/exercise2/node-http/server.js
@@ -0,0 +1,13 @@
+var http = require('http');
+
+var hostname = 'localhost';
+var port = 3000;
+var server = http.createServer(function (req,res) {
+	console.log(req.headers);
+	res.writeHead(200,{'Content-type': 'text/html'});
+	res.end('<h1>Server encoded</h1>');
+})  
+
+server.	listen(port,hostname,function(){
+	console.log(`Server running at http://${hostname}:${port}/`);
+})
