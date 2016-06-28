92d6fd3abadd0bc2506ace76a4e5f2d0355d3171
tetraquard <tstanmaysaxena@gmail.com>
2016-06-28 22:47:20 +0530

    HTTPS and FB-Passport Implementation

---
+++ week4/HTTPS_secure_communication/routes/leaderRouter.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var Leaders = require('../models/leaders');
+var Verify = require('./verify');
+
+var leaderRouter = express.Router();
+leaderRouter.use(bodyParser.json());
+
+leaderRouter.route('/')
+.get(function(req,res,next){
+	Leaders.find({}, function(err, leaders){
+		if (err) throw err;
+		res.json(leaders);
+	})
+})
+.post(Verify.verifyAdmin, function(req,res,next){
+	Leaders.create(req.body, function(err, leaders){
+		if (err) throw err;
+		console.log("Leaders Created...");
+		var id = leaders._id;
+		res.writeHead(200, {
+			'Content-type': 'text/plain'});
+			res.end('Added the promo with id: ' +  id);
+	});
+})
+.delete(Verify.verifyAdmin, function(req,res,next){
+		Leaders.remove({},function(err, resp){
+			if (err) throw err;
+			res.json(resp);
+		})
+});
+
+leaderRouter.route('/:leaderId')
+.get(function(req,res,next){
+	Leaders.findById(req.params.leaderId, function(err, leaders){
+		if (err) throw err;
+		res.json(leaders);
+	})
+})
+.put(Verify.verifyAdmin, function(req,res,next){
+	Leaders.findByIdAndUpdate(req.params.leadersId,{
+		$set:req.body
+	},{
+		new:true
+	}, function (err, leaders) {
+		res.json(leaders);
+	});
+})
+.delete(Verify.verifyAdmin, function(req,res,next){
+	Leaders.remove(req.params.leadersId, function(err, resp){
+		if (err) throw err;
+		res.json(resp)
+	});
+});
+
+module.exports = leaderRouter;
---
+++ week4/mongoose_population_facebook/models/dishes.js
@@ -0,0 +1,61 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+require('mongoose-currency').loadType(mongoose);
+var Currency = mongoose.Types.Currency;
+
+var commentSchema = new Schema({
+  rating:{
+    type:Number,
+    min:1,
+    max:5,
+    required:true
+  },
+  comment:{
+    type:String,
+    required: true
+  },
+  postedBy:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'User'
+  }
+},{
+  timestamps:true
+});
+
+var dishSchema = new Schema({
+    name:{
+      type:String,
+      required:true,
+      unique:true
+    },
+    image:{
+      type:String,
+      required:true,
+      unique:true
+    },
+    category:{
+      type:String,
+      required:true,
+      unique:true
+    },
+    price:{
+      type: Currency,
+      required:true,
+    },
+    label:{
+      type:String,
+      default: ""
+    },
+    description:{
+      type:String,
+      required:true,
+    },
+    comments:[commentSchema]},
+    {
+      timestamps:true
+});
+
+var Dishes = mongoose.model('Dish', dishSchema);
+
+module.exports = Dishes;
